9acf574c6bf7287d8d1267bf91e80e37f59c165f
JoshuaHenriques <joshuahenriques95@gmail.com>
2023-09-18 11:23:56 -0400

    create service and types

--- projects/cat-facts/service.go
+++ projects/cat-facts/service.go
@@ -1,7 +1,36 @@
 package main
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
 
 type Service interface {
-	GetCatFace(context.Context) (*CatFact, error)
+	GetCatFact(context.Context) (*CatFact, error)
+}
+
+type CatFactService struct {
+	url string
+}
+
+func NewCatFactService(url string) Service {
+	return &CatFactService{
+		url: url,
+	}
+}
+
+func (s *CatFactService) GetCatFact(ctx context.Context) (*CatFact, error) {
+	resp, err := http.Get(s.url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	fact := &CatFact{}
+	if err := json.NewDecoder(resp.Body).Decode(fact); err != nil {
+		return nil, err
+	}
+
+	return fact, nil
 }
